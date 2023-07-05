b1b0ea39a4c698c3a63f2f505d61d476d981b7ad
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-07-05 13:13:37 -0500

    oops that should have been setTimeout

--- public/script/main.js
+++ public/script/main.js
@@ -2,5 +2,5 @@ function maRead(evt) {
     let e = evt.target;
     let elt = e.parentElement.querySelector('progress-ring');
     elt.hidden = false;
-    setInterval(() => elt.setProgress(1), 10); // css transitions didn't work without this
+    setTimeout(() => elt.setProgress(1), 10); // css transitions didn't work without this
 }
