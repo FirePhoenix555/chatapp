044c93e5705631ff3ae551cc6999695f90531bd3
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-07-06 09:19:36 -0500

    fixed image urls

--- public/script/elt/message-box.js
+++ public/script/elt/message-box.js
@@ -24,7 +24,7 @@ class MessageBox extends HTMLElement {
 
             <div class="absolute right-0 top-0 m-0 p-0 flex flex-col items-end w-fit h-full">
                 <div class="menu  m-0 p-1 w-fit hover:cursor-pointer">
-                    <img src="imgs/threedots.png" class="h-5">
+                    <img src="/imgs/threedots.png" class="h-5">
                     <div class="absolute left-[100%] top-0 m-2 mt-0 p-2 pb-1 border border-black hover:cursor-auto" hidden>
                         <p class="border border-black mb-1 p-1 text-center text-sm whitespace-nowrap hover:cursor-pointer" onclick="maRead(event)">Mark as <br> read</p>
                         <div class="flex justify-center">
