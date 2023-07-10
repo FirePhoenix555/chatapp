a6dae545c5f098ff00acb92618332fa9bbef64a5
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-07-10 10:26:29 -0500

    added cookie to store if login box is open or closed

--- public/script/main.js
+++ public/script/main.js
@@ -4,3 +4,9 @@ function maRead(evt) {
     elt.hidden = false;
     setTimeout(() => elt.setProgress(1), 10); // css transitions didn't work without this
 }
+
+function exists(a) {
+    return a !== undefined && a !== null && a !== "";
+}
+
+window.getCookie = name => document.cookie.match(new RegExp(`(^| )${name}=([^;]+)`))?.at(2);
