b95dc590c18ab0eb57ee65a373395cddceeb20a5
FirePhoenix555 <53152322+FirePhoenix555@users.noreply.github.com>
2023-07-11 13:29:34 -0500

    added /api/v1/messages/ POST endpoint

--- api/server.js
+++ api/server.js
@@ -17,6 +17,10 @@ app.use(require('cors')({
 
 app.use(express.json());
 
+function genID(n, digits=8) {
+    return n.toString().padStart(digits, '0');
+}
+
 function findMessagesByRecipient(data, uid) {
     let a = [];
 
@@ -25,6 +29,13 @@ function findMessagesByRecipient(data, uid) {
     });
 
     return a;
+}
+
+function findAvailableMessageID(data) {
+    for (let i = 0; i < Object.keys(data).length + 1; i++) {
+        let id = genID(i);
+        if (!data[id]) return id;
+    }
 }
 
 async function getMessage(id, res, type) {
@@ -56,7 +67,7 @@ async function addUser(req, res, next) {
     catch { return sendError('Could not access users database', 500, res) }
     if (!data) return sendError('Could not access users database', 500, res)
 
-    const id = (Object.keys(data).length + 1).toString().padStart(5, '0');
+    const id = genID(Object.keys(data).length + 1);
     const user = {
         id: id,
         name: req.body.user,
@@ -70,10 +81,33 @@ async function addUser(req, res, next) {
     res.status(201).json(user);
 }
 
+async function addMessage(req, res, next) {
+    let data;
+    try { data = JSON.parse(fs.readFileSync('data/messages.json')); }
+    catch { return sendError('Could not access messages database', 500, res) }
+    if (!data) return sendError('Could not access messages database', 500, res)
+
+    const id = findAvailableMessageID(data);
+    const msg = {
+        id: id,
+        from: req.body.from,
+        to: req.body.to,
+        content: req.body.content,
+        time: req.body.time
+    };
+
+    data[id] = msg;
+
+    fs.writeFileSync('data/messages.json', JSON.stringify(data));
+
+    res.status(201).json(msg);
+}
+
 router.route('/api/v1/messages/id/:mid').get((req, res, next) => getMessage(req.params.mid, res, "mid"));
 router.route('/api/v1/users/:uid').get((req, res, next) => getUser(req.params.uid, res));
 router.route('/api/v1/messages/usr/:uid').get((req, res, next) => getMessage(req.params.uid, res, "uid"));
 router.route('/api/v1/users/').post(addUser);
+router.route('/api/v1/messages/').post(addMessage);
 
 app.use(router);
 
